4c8960d4d0d146d6c962eb60db788d313f6a7a9d
Elmer Melendez <elmer.melendez@gmail.com>
2024-03-11 20:07:13 -0600

    actualiza archivo javascrip

--- js/script.js
+++ js/script.js
@@ -1,18 +1,68 @@
+let message = '';
+let output = '';
+let vowels = ['a', 'e', 'i', 'o' ,'u'];
+let replacement = ['ai', 'enter', 'imes', 'ober', 'ufat'];
+
 function textAreaBackground() {
     document.getElementById('text-input').style.background = '#130d47';
     document.getElementById('text-input').style.filter = 'saturate(125%)';
 }
 
+function BlockOutputArea() {
+    document.getElementById("text-output").disabled="false";
+}
+
+function verifyMessage() {
+    message = document.getElementById('text-input').value;
+    let regex = /^[a-z\s\d[\\\]^]+$/;
+
+    if (!regex.test(message)) {
+        document.getElementById('text-input').value = '';  
+        document.getElementById('text-output').style.background = 'url(../infografico-encriptador/img/score-screen.png)'; 
+        document.getElementById('text-output').style.filter = 'saturate(200%)';    
+        document.getElementById('text-output').style.backgroundSize = 'contain';
+        document.getElementById('text-output').style.backgroundPosition = 'center';
+        alert('Solo se permiten letras minúsculas y sin acentos.')
+    }
+}
+
 function controls(controlsParameter) {
     switch (controlsParameter) {
         case 'A':
-            let initialMessage = document.getElementById('text-input').value;
-            let encryptedMessage = document.getElementById('text-output').value = initialMessage;
-            document.getElementById('text-output').style.background = '#000000';
-            console.log(initialMessage);  
-        break;
+
+            verifyMessage()
+            message = document.getElementById('text-input').value;
+            output = '';
+
+            for (let i = 0; i < message.length; i++) {
+                let character = message[i];
+                if (vowels.includes(character)) {
+                    let vowelsIndex = vowels.indexOf(character);
+                    output += replacement[vowelsIndex];
+                } else {
+                    output += character;
+                }     
+            }
+
+            if (output !== "") {
+                document.getElementById('text-output').value = output;
+                document.getElementById('text-output').style.background = '#000000';                
+            }
+
+            break;    
         case 'B':
-            alert('Soy el Boton B');               
+
+            verifyMessage()
+            message = document.getElementById('text-input').value.toLowerCase();       
+
+            for (let i = 0; i < message.length; i++) {
+                if (message.includes(replacement[i])) {
+                    message = message.replaceAll(replacement[i], vowels[i]);  
+                } 
+            }
+
+            document.getElementById('text-output').value = message;
+            
             break;              
         case 'Reset':
             document.getElementById('text-input').value = '';  
@@ -24,8 +74,25 @@ function controls(controlsParameter) {
             document.getElementById('text-output').style.background = 'url(../infografico-encriptador/img/score-screen.png)'; 
             document.getElementById('text-output').style.filter = 'saturate(200%)';    
             document.getElementById('text-output').style.backgroundSize = 'contain';
+            document.getElementById('text-output').style.backgroundPosition = 'center';
             break;
         default:
             break;
     }
 }
+
+function buttonDelete() {
+    document.getElementById('text-output').value = '';  
+    document.getElementById('text-output').style.background = 'url(../infografico-encriptador/img/score-screen.png)'; 
+    document.getElementById('text-output').style.filter = 'saturate(200%)';    
+    document.getElementById('text-output').style.backgroundSize = 'contain';
+    document.getElementById('text-output').style.backgroundPosition = 'center';
+}
+
+function buttonCopy() {
+    let copyText = document.getElementById('text-output');
+    navigator.clipboard.writeText(copyText.value);
+}
+
+BlockOutputArea();
+
